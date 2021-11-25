cb8028fa8e81a7598151a8d8f3f26dd298a99e4d
Dilshodbek <20020905xd@gmail.com>
2021-11-25 16:03:43 +0500

    commited

---
+++ buttons.py
@@ -0,0 +1,56 @@
+from telebot import types
+
+
+def langs():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    uz = types.InlineKeyboardButton('Uzb', callback_data='uzb')
+    ru = types.InlineKeyboardButton('Rus', callback_data='rus')
+    markup.add(ru, uz)
+    return markup
+
+
+def button_start():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    subj = types.InlineKeyboardButton('Fanlar', callback_data='subjects')
+    lang = types.InlineKeyboardButton('Tillar', callback_data='languages')
+    markup.add(subj, lang)
+    return markup
+
+
+def buttons_subjects():
+    markup = types.InlineKeyboardMarkup(row_width=4)
+    python = types.InlineKeyboardButton('python', callback_data='python')
+    js = types.InlineKeyboardButton('js', callback_data='js')
+    math = types.InlineKeyboardButton('matem', callback_data='math')
+    history = types.InlineKeyboardButton('Tarix', callback_data='history')
+    smm = types.InlineKeyboardButton('smm🎯', callback_data='smm')
+    literature = types.InlineKeyboardButton('literature', callback_data='literature')
+    chemistry = types.InlineKeyboardButton('chemistry', callback_data='chemistry')
+    java = types.InlineKeyboardButton('java', callback_data='java')
+    graphic_design = types.InlineKeyboardButton('grafik design', callback_data='graphic_design')
+    ai = types.InlineKeyboardButton('ai', callback_data='ai')
+    ielts = types.InlineKeyboardButton('ielts', callback_data='ielts')
+    frontend = types.InlineKeyboardButton('frontend', callback_data='frontend')
+    php = types.InlineKeyboardButton('php', callback_data='php')
+    ios = types.InlineKeyboardButton('ios', callback_data='ios')
+    flutter = types.InlineKeyboardButton('flutter', callback_data='flutter')
+    fizika = types.InlineKeyboardButton('fizika', callback_data='fizika')
+    biology = types.InlineKeyboardButton('biology🧪', callback_data='biology')
+    robototehnika = types.InlineKeyboardButton('🤖 tehnika', callback_data='robototehnika')
+    mobile_developing = types.InlineKeyboardButton('📱 developing', callback_data='mobile_developing')
+    markup.add(python, js, math, history, smm, fizika, flutter, php, ios, frontend, biology, robototehnika, literature, ielts, chemistry, ai, java, graphic_design, mobile_developing)
+    return markup
+
+
+def buttons_langs():
+    markup = types.InlineKeyboardMarkup(row_width=3)
+    english = types.InlineKeyboardButton('english🇬🇧', callback_data='english')
+    russian = types.InlineKeyboardButton('russian🇷🇺', callback_data='russian')
+    korean = types.InlineKeyboardButton('korean🇰🇷', callback_data='korean')
+    japan = types.InlineKeyboardButton('japan🇯🇵', callback_data='japan')
+    arabic = types.InlineKeyboardButton('arabic🇦🇪', callback_data='arabic')
+    deutsch = types.InlineKeyboardButton('deutsch🇩🇪', callback_data='deutsch')
+    french = types.InlineKeyboardButton('french🇫🇷', callback_data='french')
+    chinese = types.InlineKeyboardButton('chinese🇨🇳', callback_data='chinese')
+    markup.add(english, russian, korean, japan, arabic, deutsch, french, chinese)
+    return markup
---
+++ main.py
@@ -0,0 +1,83 @@
+from telebot import types
+import telebot
+import requests
+import buttons
+
+URL = 'http://18.191.130.122/apicentres/'
+URL_LANGS = 'http://learningcourses.uz/apicentres/?lang__slugs={}&sub__slugs='
+URL_SUBJS = 'http://learningcourses.uz/apicentres/?lang__slugs=&sub__slugs={}'
+BOT_TOKEN = "2144753786:AAEVx-4NRLrj0cugcS6kPeMkuaFbv2Wm0rw"
+bot = telebot.TeleBot(BOT_TOKEN)
+users = []
+
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(f'{local_filename}', 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
+
+
+def getCentres(message, data):
+    for i in data:
+        result = ''
+        ln = ''
+        image_url = i['img']
+        more = f"http://learningcourses.uz/detailed/{i['id']}/"
+        download_file(image_url)
+        photo_text = image_url.split('/')[-1]
+        photo = open(f'{photo_text}', 'rb')
+        for lang in i['lang']:
+            ln += f"🔷{lang['name']}\n"
+        sb = ''
+        for subj in i['sub']:
+            sb += f"🔶{subj['name']}\n"
+        result += f"🎓🎓🎓 {i['name']} 🎓🎓🎓 \n\n🚩Languages: \n{ln} \n\n📚Subjects: \n{sb} \n✅{i['additional']} \nMore info: {more} \n\n📲Tel: +998 {i['tel']} \n📥Sayt: {i['sayt']} \n🗺{i['addres']}\n\n Likes:👍🏻 {len(list(i['likes']))}\t Dislikes:👎🏻 {len(list(i['dis_likes']))} \n\n \n👉👉 @learning_courses_bot 👈👈 "
+        bot.send_photo(message.from_user.id, photo, caption=result)
+
+
+def centresbyslug(message, slug):
+    try:
+        centres = requests.get(URL_SUBJS.format(slug)).json()
+        if centres:
+            getCentres(message, centres)
+        else:
+            centres = requests.get(URL_LANGS.format(f'{slug}')).json()
+            getCentres(message, centres)
+    except:
+        print('error')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.from_user.id, 'Assalom aleykum qanaqa yonalishda O\'quv markaz izlayabsiz',
+                     reply_markup=buttons.button_start())
+
+
+@bot.message_handler(commands=['fanlar'])
+def start_message(message):
+    bot.send_message(message.from_user.id, 'Qaysi yo\'nalishda kurslar qidiryabsiz \n📚📙📘💻📱',
+                     reply_markup=buttons.buttons_subjects())
+
+
+@bot.message_handler(commands=['tillar'])
+def start_message(message):
+    bot.send_message(message.from_user.id, 'Qaysi Tildan kurslar qidiryabsiz \n🇬🇧 🇷🇺 🇰🇷 🇯🇵 🇦🇪 🇩🇪 🇫🇷 🇨🇳',
+                     reply_markup=buttons.buttons_langs())
+
+
+@bot.callback_query_handler(func=lambda m: True)
+def StudyCentres(call):
+    if call.data == 'subjects':
+        bot.send_message(call.from_user.id, 'Qaysi yo\'nalishda kurslar qidiryabsiz \n📚📙📘💻📱',
+                         reply_markup=buttons.buttons_subjects())
+    elif call.data == 'languages':
+        bot.send_message(call.from_user.id, 'Qaysi Tildan kurslar qidiryabsiz \n🇬🇧 🇷🇺 🇰🇷 🇯🇵 🇦🇪 🇩🇪 🇫🇷 🇨🇳',
+                         reply_markup=buttons.buttons_langs())
+    else:
+        centresbyslug(call, call.data)
+
+
+bot.polling()
